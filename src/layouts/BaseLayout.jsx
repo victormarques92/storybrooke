b5ade39ad3de3816cc118ab1776da53abe637f15
Victor Marques <victormatheus92@gmail.com>
2025-06-25 12:15:13 -0300

    update

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -3,12 +3,12 @@ import { Header } from '../components/Header';
 
 export default function BaseLayout({ children }) {
   return (
-    <div>
+    <>
       <Header />
 
       <main className="min-h-[calc(100vh-140px)] p-8">{children}</main>
 
       <Footer />
-    </div>
+    </>
   );
 }
